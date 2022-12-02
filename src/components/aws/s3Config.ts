c0fa8a85233bd92999acb7930657aba87cc43073
Edgardo Vittoria <37239644+edgardovittoria@users.noreply.github.com>
2022-12-02 17:34:52 +0100

    Revert "Aws s3 test"

--- src/components/aws/s3Config.ts
+++
@@ -1,7 +0,0 @@
-export const s3Config = {
-    bucketName: "esymia-storage",
-    region: "us-east-1",
-    accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY as string,
-    secretAccessKey: process.env.REACT_APP_AWS_SECRET_KEY as string,
-    s3Url: "https://esymia-storage.s3.amazonaws.com/",
-}
--- src/components/faunadb/api/modelsAPIs.ts
+++ src/components/faunadb/api/modelsAPIs.ts
@@ -1,7 +1,6 @@
 import toast from "react-hot-toast";
 import { ComponentEntity } from "../../model/componentEntity/componentEntity";
 import faunadb from "faunadb"
-import {uploadFileS3} from "../../aws/projectsAPIs";
 
 export type FaunaCadModel = {
     name: string,
@@ -13,27 +12,20 @@ export type FaunaCadModel = {
 
 export async function saveNewModel(faunaClient: faunadb.Client, faunaQuery: typeof faunadb.query, newModel: FaunaCadModel) {
     try {
-        let file: File = new File([JSON.stringify(newModel.components)], `${newModel.name}.json`)
-        uploadFileS3(file).then((res: any) => {
-            faunaClient.query((
-                faunaQuery.Create(
-                    faunaQuery.Collection('CadModels'),
-                    {
-                        data: {
-                            ...newModel,
-                            components: res.Response.location //TODO: test if it is the correct path to location
-                        }
+        await faunaClient.query((
+            faunaQuery.Create(
+                faunaQuery.Collection('CadModels'),
+                {
+                    data: {
+                        ...newModel
                     }
-                )
-            )).then(() => toast.success("Model successfully saved!"))
-                .catch((e) => {
-                    toast.error("Model not saved! See console log for error details.")
-                    console.log(e)
-                })
-        })
+                }
+            )
+        ))
+        toast.success("Model successfully saved!")
     } catch (e) {
-        //toast.error("Model not saved! See console log for error details.")
-        //console.log(e)
+        toast.error("Model not saved! See console log for error details.")
+        console.log(e)
     }
 }
 
